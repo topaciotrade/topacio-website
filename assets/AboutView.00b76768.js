756f5dc1b86e3a22e53212ec810456201d8ddd85
Julio Vinachi <julio.vinachi@payco.co>
2022-02-26 22:45:59 -0400

    feat: control refactor

--- assets/AboutView.00b76768.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.bcddbdd2.js";import{o as t,c as o,b as _}from"./vendor.6d38d6db.js";const s={},a={class:"about"},c=_("h1",null,"This is an about page",-1),n=[c];function r(i,u){return t(),o("div",a,n)}var f=e(s,[["render",r]]);export{f as default};
